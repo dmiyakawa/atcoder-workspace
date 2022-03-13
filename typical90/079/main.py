7ff91c775bd9c337543475b15c9a79fb07735044
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-13 19:24:16 +0900

    Typical90 079

--- typical90/079/main.py
+++ typical90/079/main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 
-YES = "Yes"  # type: str
-NO = "No"  # type: str
-
-
 def main():
     import sys
 
@@ -22,7 +18,27 @@ def main():
 
 
 def solve(H: int, W: int, A: "List[List[int]]", B: "List[List[int]]"):
-    return
+    C = [[A[i][j] - B[i][j] for j in range(W)] for i in range(H)]
+    count = 0
+    possible = True
+    for i in range(H - 1):
+        for j in range(W - 1):
+            val = C[i][j]
+            if val != 0:
+                count += abs(val)
+                C[i][j] -= val
+                C[i + 1][j] -= val
+                C[i + 1][j + 1] -= val
+                C[i][j + 1] -= val
+        if C[i][W - 1] != 0:
+            possible = False
+            break
+    if possible:
+        for j in range(W):
+            if C[H - 1][j] != 0:
+                possible = False
+            break
+    print(f"Yes\n{count}" if possible else "No")
 
 
 if __name__ == "__main__":
