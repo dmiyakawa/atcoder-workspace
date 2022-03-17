c941f6f7cdd655d109c7d7c0a9a9c91a14d3a4d6
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-17 22:57:25 +0900

    Modify impl for Typical 90 020

--- typical90/020/main.py
+++ typical90/020/main.py
@@ -17,14 +17,15 @@ def main():
     a = int(next(tokens))  # type: int
     b = int(next(tokens))  # type: int
     c = int(next(tokens))  # type: int
-    solve2(a, b, c)
+    solve(a, b, c)
 
 
 def solve(a: int, b: int, c: int):
     print(YES if a < c ** b else NO, end="")
 
 
-def solve2(a: int, b: int, c: int):
+def solve_wrong(a: int, b: int, c: int):
+    # 誤差が発生してWAが出る実装。サンプルとして実際のテストデータからin_ex_1.txtとout_ex_1.txtも付属
     import math
     print(YES if math.log2(a) < b * math.log2(c) else NO, end="")
 
