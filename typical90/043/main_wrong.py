391a7cbe6b53002fce03bcc877dd3d956645928c
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-17 20:35:15 +0900

    Typical90 043 WA

--- typical90/043/main_wrong.py
+++ typical90/043/main_wrong.py
@@ -39,41 +39,40 @@ def main():
             print("".join(lst))
         print()
 
-    not_tried = [(0, sr, sc, LEFT), (0, sr, sc, RIGHT), (0, sr, sc, UP), (0, sr, sc, DOWN)]
+    distance = abs(sr - tr) + abs(sc - tc)
+    not_tried = [(0, distance, 0, sr, sc, LEFT),
+                 (0, distance, 0, sr, sc, RIGHT),
+                 (0, distance, 0, sr, sc, UP),
+                 (0, distance, 0, sr, sc, DOWN)]
     heapq.heapify(not_tried)
 
-    dp: Dict[Tuple[int, int, str], int] = {(sr, sc, LEFT): 0, (sr, sc, RIGHT): 0,
-                                           (sr, sc, UP): 0, (sr, sc, DOWN): 0}
-    current_minimum_t = MAX_VALUE
+    dp: Dict[Tuple[int, int, str], int] = {(sr, sc): 0}
     ended = False
     while not_tried:
         if ended:
             break
-        t, r, c, d = heapq.heappop(not_tried)
-        if current_minimum_t < t:
-            break
+        t, distance, same_direction, r, c, d = heapq.heappop(not_tried)
         # _debug(r, c, d)
-
-        def check(tup):
-            r2, c2, d2, t2 = tup
-            return 0 <= r2 < H and 0 <= c2 < W and dp.get((r2, c2, d2), MAX_VALUE) > t2 and maze[r2][c2] == "."
-
         if d == LEFT:
-            next_moves = filter(check, [(r, c - 1, LEFT, t), (r - 1, c, UP, t + 1), (r + 1, c, DOWN, t + 1)])
+            next_moves = [(r, c - 1, LEFT, t), (r - 1, c, UP, t + 1), (r + 1, c, DOWN, t + 1)]
         elif d == RIGHT:
-            next_moves = filter(check, [(r, c + 1, RIGHT, t), (r - 1, c, UP, t + 1), (r + 1, c, DOWN, t + 1)])
+            next_moves = [(r, c + 1, RIGHT, t), (r - 1, c, UP, t + 1), (r + 1, c, DOWN, t + 1)]
         elif d == UP:
-            next_moves = filter(check, [(r - 1, c, UP, t), (r, c - 1, LEFT, t + 1), (r, c + 1, RIGHT, t + 1)])
+            next_moves = [(r - 1, c, UP, t), (r, c - 1, LEFT, t + 1), (r, c + 1, RIGHT, t + 1)]
         else:
-            next_moves = filter(check, [(r + 1, c, DOWN, t), (r, c - 1, LEFT, t + 1), (r, c + 1, RIGHT, t + 1)])
+            next_moves = [(r + 1, c, DOWN, t), (r, c - 1, LEFT, t + 1), (r, c + 1, RIGHT, t + 1)]
 
         for (next_r, next_c, next_d, next_t) in next_moves:
-            if dp.get((next_r, next_c, next_d), MAX_VALUE) > next_t and current_minimum_t > next_t:
+            if next_r < 0 or H <= next_r or next_c < 0 or W <= next_c or maze[next_r][next_c] == "#":
+                continue
+            if dp.get((next_r, next_c, next_d), MAX_VALUE) > next_t:
                 dp[(next_r, next_c, next_d)] = next_t
                 if (next_r, next_c) == (tr, tc):
-                    current_minimum_t = next_t
-                # _debug(next_r, next_c, next_t)
-                heapq.heappush(not_tried, (next_t, next_r, next_c, next_d))
+                    ended = True
+                    break
+                next_distance = abs(next_r - tr) + abs(next_c - tc)
+                same_direction = 0 if next_d == d else 1
+                heapq.heappush(not_tried, (next_t, next_distance, same_direction, next_r, next_c, next_d))
     print(min(dp.get((tr, tc, LEFT), MAX_VALUE),
               dp.get((tr, tc, UP), MAX_VALUE),
               dp.get((tr, tc, RIGHT), MAX_VALUE),
