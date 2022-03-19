d6709596d9af64d296fd843e4aa443ec1899ec6c
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-19 17:11:22 +0900

    Typical90 046 AC

--- typical90/046/main.py
+++ typical90/046/main.py
@@ -18,7 +18,18 @@ def main():
 
 
 def solve(N: int, A: "List[int]", B: "List[int]", C: "List[int]"):
-    return
+    Am, Bm, Cm = {}, {}, {}
+    for a, b, c in zip(A, B, C):
+        Am[a % 46] = Am.get(a % 46, 0) + 1
+        Bm[b % 46] = Bm.get(b % 46, 0) + 1
+        Cm[c % 46] = Cm.get(c % 46, 0) + 1
+    count = 0
+    for a in range(0, 46):
+        for b in range(0, 46):
+            for c in range(0, 46):
+                if (a + b + c) % 46 == 0:
+                    count += Am.get(a, 0) * Bm.get(b, 0) * Cm.get(c, 0)
+    print(count, end="")
 
 
 if __name__ == "__main__":
