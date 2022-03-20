bcdfc37bb594c5524d8659dd3134f6a7b0948783
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-20 18:06:12 +0900

    Typical90 038 AC

--- typical90/038/main.py
+++ typical90/038/main.py
@@ -2,21 +2,21 @@
 
 
 def main():
+    import math
     import sys
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    A = int(next(tokens))  # type: int
-    B = int(next(tokens))  # type: int
-    solve(A, B)
-
-
-def solve(A: int, B: int):
-    return
+    A, B = [int(e) for e in sys.stdin.readline().split()]
+    muls = 1
+    a, b = A, B
+    while True:
+        gcd = math.gcd(a, b)
+        if gcd == 1:
+            break
+        muls *= gcd
+        a = A // muls
+        b = B // muls
+    lcd = a * b * muls
+    ans = "Large" if lcd > 10 ** 18 else lcd
+    print(ans, end="")
 
 
 if __name__ == "__main__":
