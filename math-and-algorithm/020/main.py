db4ef6ee712af9abe7d3a3ea790143d83489ce88
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-03-25 19:29:09 +0900

    math-and-algorithms 020

--- math-and-algorithm/020/main.py
+++ math-and-algorithm/020/main.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
+import array
 
-
-def main():
-    import sys
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    N = int(next(tokens))  # type: int
-    A = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
-    solve(N, A)
-
-
-def solve(N: int, A: "List[int]"):
-    return
-
-
-if __name__ == "__main__":
-    main()
+N = int(input())
+A = array.array("l", [int(e) for e in input().split()])
+count = 0
+for i, a in enumerate(A):
+    for j, b in enumerate(A[i + 1:], start=i + 1):
+        if a + b > 1000:
+            continue
+        for k, c in enumerate(A[j + 1:], start=j + 1):
+            if a + b + c > 1000:
+                continue
+            for l, d in enumerate(A[k + 1:], start=k + 1):
+                if a + b + c + d > 1000:
+                    continue
+                for e in A[l + 1:]:
+                    if a + b + c + d + e == 1000:
+                        count += 1
+print(count)
