485fa1930be783836dc3eebb1a7dd17dff8055bf
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-04-02 12:59:56 +0900

    dp E

--- dp/E/main.py
+++ dp/E/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from typing import List, Tuple, Union
 
 input = sys.stdin.readline
 sys.setrecursionlimit(2 * (10 ** 5))
@@ -28,7 +29,20 @@ def main():
 
 
 def solve(N: int, W: int, w: "List[int]", v: "List[int]"):
-    return
+    V = 10**3 * N + 1
+    items = list(zip(w, v))
+    dp: List[List[Union[int, float]]] = [[INF for _ in range(V)]]
+    dp[0][0] = 0
+    max_value = 0
+    for i in range(1, N + 1):
+        dp.append(dp[i - 1].copy())
+        weight, value = items[i - 1]
+        for j in range(V):
+            if dp[i - 1][j - value] + weight <= W:
+                dp[i][j] = min(dp[i - 1][j - value] + weight, dp[i - 1][j])
+                max_value = max(max_value, j)
+
+    print(max_value)
 
 
 if __name__ == "__main__":
