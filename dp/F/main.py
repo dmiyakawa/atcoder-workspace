18977e6af3a710336861d864a1535a2e9261f44c
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-04-02 19:39:07 +0900

    dp F, G

--- dp/F/main.py
+++ dp/F/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from typing import List, Optional
 
 input = sys.stdin.readline
 sys.setrecursionlimit(2 * (10 ** 5))
@@ -23,7 +24,28 @@ def main():
 
 
 def solve(s: str, t: str):
-    return
+    dp: List[List[int]] = [[0] * (len(t) + 1)]
+    for i in range(1, len(s) + 1):
+        dp.append(dp[i - 1].copy())
+        for j in range(1, len(t) + 1):
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    length = dp[len(s)][len(t)]
+    ans: List[Optional[str]] = [None] * length
+    cur_s, cur_t = len(s), len(t)
+    while length > 0:
+        if s[cur_s - 1] == t[cur_t - 1]:
+            ans[length - 1] = t[cur_t - 1]
+            length -= 1
+            cur_s -= 1
+            cur_t -= 1
+        elif dp[cur_s - 1][cur_t] == dp[cur_s][cur_t]:
+            cur_s -= 1
+        else:
+            cur_t -= 1
+    print("".join(ans))
 
 
 if __name__ == "__main__":
--- dp/G/main.py
+++ dp/G/main.py
@@ -27,8 +27,31 @@ def main():
     solve(N, M, x, y)
 
 
-def solve(N: int, M: int, x: "List[int]", y: "List[int]"):
-    return
+def solve(N: int, M: int, X: "List[int]", Y: "List[int]"):
+    links = {}
+    for x, y in zip(X, Y):
+        links.setdefault(x - 1, set()).add(y - 1)
+
+    lens = [0] * N
+    visited = [False] * N
+
+    def get_length(node_i):
+        if visited[node_i]:
+            return lens[node_i]
+        next_nodes = links.get(node_i, set())
+        if next_nodes:
+            l = max(get_length(next_node_i) for next_node_i in next_nodes) + 1
+        else:
+            l = 0
+        visited[node_i] = True
+        lens[node_i] = l
+        return l
+
+    max_len = 0
+    for i in range(N):
+        max_len = max(get_length(i), max_len)
+
+    print(max_len)
 
 
 if __name__ == "__main__":
