8496d30629099d5c9c13785b0965f0903c05fe86
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-05-18 19:07:50 +0900

    Updates

--- abc002/A/main.py
+++ abc002/A/main.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(2 * (10 ** 5))
-INF = float("INF")
-
-
 def main():
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    X = int(next(tokens))  # type: int
-    Y = int(next(tokens))  # type: int
-    solve(X, Y)
-
-
-def solve(X: int, Y: int):
-    return
+    pass
 
 
 if __name__ == "__main__":
