3e0caba578af73ff5d462f602f3dc01473f391c4
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-06-22 22:44:14 +0900

    Updates

--- keyence2020/C/main.py
+++ keyence2020/C/main.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
-
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
-
 
 def solve(N: int, K: int, S: int):
-    return
+    lst = [S] * K
+    rem = N - K
+    if S == 10**5:
+        lst.extend([10**5 - 1] * rem)
+    elif S < 10**5:
+        lst.extend([10**5] * rem)
+    else:
+        lst.extend([1] * rem)
+    return lst
 
 
 def main():
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    N = int(next(tokens))  # type: int
-    K = int(next(tokens))  # type: int
-    S = int(next(tokens))  # type: int
-    solve(N, K, S)
+    N, K, S = map(int, input().split())
+    print(*solve(N, K, S))
 
 
 if __name__ == "__main__":
