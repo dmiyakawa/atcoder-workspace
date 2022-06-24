5a09ae993de4ebe7c3a8ef60251d599c144f117e
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-06-24 15:38:35 +0900

    Updates

--- abc172/D/main.py
+++ abc172/D/main.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 
-import sys
+def factorize_in_prime(n) -> "Dict[int, int]":
+    """2以上の整数nを素因数分解し、{素因数: 指数, ...}の辞書を返す"""
+    # https://qiita.com/snow67675476/items/e87ddb9285e27ea555f8
+    assert n >= 2
+    d = {}
+    temp = n
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:
+            count = 0
+            while temp % i == 0:
+                count += 1
+                temp //= i
+            d[i] = count
 
-input = sys.stdin.readline
-sys.setrecursionlimit(2 * (10 ** 5))
-INF = float("INF")
-MOD = 10 ** 9 + 7
-MOD2 = 998244353
+    if temp != 1:
+        d[temp] = 1
+
+    if not d:
+        d[n] = 1
+
+    return d
 
 
 def main():
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    N = int(next(tokens))  # type: int
-    solve(N)
-
-
-def solve(N: int):
-    return
+    N = int(input())
+    count = 0
+    for n in range(1, N + 1):
+        if n == 1:
+            count += 1
+            continue
+        d = factorize_in_prime(n)
 
 
 if __name__ == "__main__":
