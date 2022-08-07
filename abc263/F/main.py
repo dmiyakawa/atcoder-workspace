3ebcd0c98386df13fe1de8bb8bc8c55a9197f318
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-08-07 22:39:16 +0900

    Updates

--- abc263/F/main.py
+++ abc263/F/main.py
@@ -1,13 +1,158 @@
 #!/usr/bin/env python3
-
-import sys
-
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
+import copy
+from typing import Callable, Any, List, Union
 
 
 def main():
-    x = input()
+    N = int(input())
+    C = [[int(e) for e in input().split()] for _ in range(2**N)]
+    Cac = copy.deepcopy(C)
+    for i in range(2**N):
+        for n in range(1, N):
+            Cac[i][n] += Cac[i][n - 1]
+    dp = [SegTree(op=lambda a, b: max(a, b), e=0, v=2**N) for _ in range(N)]
+    # 1回戦 (index 0)
+    for i in range(2**N):
+        dp[0].set(i, C[i][0])
+
+    # 2回戦以降 (index [1, N-1])
+    for n in range(1, N):
+        for i in range(0, 2**N):
+            b_index = i // 2**n
+            cb_index = (b_index // 2) * 2 + ((b_index % 2 + 1) % 2)
+            start = cb_index * 2**n
+            end = start + 2**n
+            prod = dp[n - 1].prod(start, end)
+            v = C[i][n] - C[i][n - 1] + dp[n - 1].get(i) + prod
+            # print(f"n: {n}, i: {i}, bi-cbi: {b_index}, {cb_index}, s-e: {start}, {end}")
+            # print(f"  C[i][n]: {C[i][n]}, C[i][n - 1]: {C[i][n - 1]}, prod: {prod}")
+            # print(f"  v: {v}")
+            dp[n].set(i, v)
+    print(dp[N - 1].all_prod())
+
+
+class SegTree:
+    def __init__(
+        self,
+        op: "Callable[[Any, Any], Any]",
+        e: "Any",
+        v: "Union[int, List[Any]]",
+    ) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = self.__class__._ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    @staticmethod
+    def _ceil_pow2(n: int) -> int:
+        x = 0
+        while (1 << x) < n:
+            x += 1
+        return x
+
+    def set(self, p: int, x: "Any") -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> "Any":
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> "Any":
+        assert 0 <= left <= right <= self._n
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> "Any":
+        return self._d[1]
+
+    def max_right(self, left: int, f: "Callable[[Any], bool]") -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, f: "Callable[[Any], bool]") -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
 
 
 if __name__ == "__main__":
