6ec401f312480f0165e8801f5e99733ee9801f56
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-08-07 17:43:37 +0900

    Updates

--- abc263/D/main.py
+++ abc263/D/main.py
@@ -1,150 +1,32 @@
 #!/usr/bin/env python3
 
 import sys
-from bisect import bisect_left, bisect_right
-from typing import Generic, Iterable, Iterator, TypeVar, Union, List
 
-T = TypeVar("T")
+def solve_editorial(N: int, L: int, R: int, A: "List[int]"):
+    """https://atcoder.jp/contests/abc263/editorial/4549
+    Lで置き換える操作とRと置き換える操作を同時に行うindexに意味はない
+    よって「LとRの操作は重ならない」と仮定して問題はない。
+    Lで「置き換えても良い」範囲とRで「置き換えても良い」範囲を仕切りのように0〜Nで移動させたときどこかの仕切りで最小になる
+    Lで「途中まで置き換えても良い」操作による最小値をf(x)、
+    Rで「途中まで置き換えても良い」操作による最小値をg(x)としたとき、上記の解説のような定式化となる。
+    f(0)...f(N), g(0)...g(N)を求める操作をO(N)で行い、
+    それを用いて仕切りを移動させつつ最小値を求める操作をO(N)で行うので、全体でO(N)
+    """
 
-
-class SortedSet(Generic[T]):
-    BUCKET_RATIO = 50
-    REBUILD_RATIO = 170
-
-    def _build(self, a=None) -> None:
-        """Evenly divides `a` into buckets."""
-        if a is None: a = list(self)
-        size = self.size = len(a)
-        bucket_size = int(math.ceil(math.sqrt(size / self.BUCKET_RATIO)))
-        self.a = [a[size * i // bucket_size: size * (i + 1) // bucket_size] for i in range(bucket_size)]
-
-    def __init__(self, a: Iterable[T] = tuple()) -> None:
-        """\
-        Makes a new SortedSet from iterable.
-        O(N) if sorted and unique. O(N log N) otherwise.
-        """
-        a = list(a)
-        if not all(a[i] < a[i + 1] for i in range(len(a) - 1)):
-            a = sorted(set(a))
-        self._build(a)
-
-    def __iter__(self) -> Iterator[T]:
-        for i in self.a:
-            for j in i: yield j
-
-    def __reversed__(self) -> Iterator[T]:
-        for i in reversed(self.a):
-            for j in reversed(i): yield j
-
-    def __len__(self) -> int:
-        return self.size
-
-    def __repr__(self) -> str:
-        return "SortedSet" + str(self.a)
-
-    def __str__(self) -> str:
-        s = str(list(self))
-        return "{" + s[1: len(s) - 1] + "}"
-
-    def _find_bucket(self, x: T) -> List[T]:
-        """\
-        Finds the bucket which should contain x.
-        self.a must not be empty.
-        """
-        for a in self.a:
-            if x <= a[-1]:
-                return a
-        return a
-
-    def __contains__(self, x: T) -> bool:
-        if self.size == 0: return False
-        a = self._find_bucket(x)
-        i = bisect_left(a, x)
-        return i != len(a) and a[i] == x
-
-    def add(self, x: T) -> bool:
-        """\
-        Adds an element and return True if added. / O(√N)
-        """
-        if self.size == 0:
-            self.a = [[x]]
-            self.size = 1
-            return True
-        a = self._find_bucket(x)
-        i = bisect_left(a, x)
-        if i != len(a) and a[i] == x: return False
-        a.insert(i, x)
-        self.size += 1
-        if len(a) > len(self.a) * self.REBUILD_RATIO:
-            self._build()
-        return True
-
-    def discard(self, x: T) -> bool:
-        """\
-        Removes an element and return True if removed. / O(√N)
-        """
-        if self.size == 0: return False
-        a = self._find_bucket(x)
-        i = bisect_left(a, x)
-        if i == len(a) or a[i] != x: return False
-        a.pop(i)
-        self.size -= 1
-        if len(a) == 0: self._build()
-        return True
-
-    def lt(self, x: T) -> Union[T, None]:
-        """Find the largest element that is less than x, or None if it doesn't exist."""
-        for a in reversed(self.a):
-            if a[0] < x:
-                return a[bisect_left(a, x) - 1]
-
-    def le(self, x: T) -> Union[T, None]:
-        """Find the largest element <= x, or None if it doesn't exist."""
-        for a in reversed(self.a):
-            if a[0] <= x:
-                return a[bisect_right(a, x) - 1]
-
-    def gt(self, x: T) -> Union[T, None]:
-        """Find the smallest element > x, or None if it doesn't exist."""
-        for a in self.a:
-            if a[-1] > x:
-                return a[bisect_right(a, x)]
-
-    def ge(self, x: T) -> Union[T, None]:
-        """Find the smallest element >= x, or None if it doesn't exist."""
-        for a in self.a:
-            if a[-1] >= x:
-                return a[bisect_left(a, x)]
-
-    def __getitem__(self, x: int) -> T:
-        """Return the x-th element, or IndexError if it doesn't exist."""
-        if x < 0: x += self.size
-        if x < 0: raise IndexError
-        for a in self.a:
-            if x < len(a): return a[x]
-            x -= len(a)
-        raise IndexError
-
-    def index(self, x: T) -> int:
-        """Count the number of elements that are less than x"""
-        ans = 0
-        for a in self.a:
-            if a[-1] >= x:
-                return ans + bisect_left(a, x)
-            ans += len(a)
-        return ans
-
-    def index_right(self, x: T) -> int:
-        """Count the number of elements <= x."""
-        ans = 0
-        for a in self.a:
-            if a[-1] > x:
-                return ans + bisect_right(a, x)
-            ans += len(a)
-        return ans
+    f = [0 for _ in range(N+1)]
+    g = [0 for _ in range(N+1)]
+    for n in range(1, N+1):
+        f[n] = min(f[n - 1] + A[n - 1], L * n)
+        g[n] = min(g[n - 1] + A[N - n], R * n)
+    print(min(f[n] + g[N - n] for n in range(0, N+1)))
 
 
-def solve(N: int, L: int, R: int, A: "List[int]"):
+def solve_wa(N: int, L: int, R: int, A: "List[int]"):
+    """\
+    コンテスト時。LとRの範囲が「食い合う」状態が発生してしまって対応に苦慮した
+    - 累積和は負値に対してあまり良い見通しを与えない。定数値で置き換える場合はさらにそう
+    - 「LとRの操作が重なる」ケースを考えすぎ。そもそも本質的には重なるわけではない
+    """
     Aac = []
     for i, a in enumerate(A):
         Aac.append(a + (Aac[i - 1] if i > 0 else 0))
@@ -201,7 +83,7 @@ def main():
     L = int(next(tokens))  # type: int
     R = int(next(tokens))  # type: int
     A = [int(next(tokens)) for _ in range(N)]  # type: "List[int]"
-    solve(N, L, R, A)
+    solve_editorial(N, L, R, A)
 
 
 if __name__ == "__main__":
--- abc263/E/main.py
+++ abc263/E/main.py
@@ -1,16 +1,204 @@
 #!/usr/bin/env python3
 
 import sys
-
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
-
+from typing import Union, Any, List
 
 MOD = 998244353  # type: int
 
 
-def solve(N: int, A: "List[int]"):
-    return
+def solve_ref(N: int, A: "List[int]"):
+    """\
+    - https://atcoder.jp/contests/abc263/editorial/4546
+    - https://atcoder.jp/contests/abc263/submissions/33857814
+    - mod付き割り算が前提。フェルマーの小定理ベースの実装として割り算を掛け算に変換するのを忘れない
+    - 無限等比級数の公式のひとつとして r + 2*r^2 + 3*r^3 + ... = r / (1 - r)^2 (https://mathwords.net/mugenwa 等)
+    - 上記よりXつまり「サイコロで0を出す回数の期待値」がもとまる。ちなみに確率aの事象が起こるまでの総回数の期待値はそもそも1/aでおなじ。
+    - A[i] が大きい場合に O(N^2) になるため、sum() 部分は工夫する。以下の実装ではseg木
+    """
+    mc = ModComb(1, p=MOD)
+    ss = SegTree(op=lambda a, b: a + b, e=0, v=N)
+    for i in range(N - 2, -1, -1):
+        v = (ss.prod(i + 1, i + A[i] + 1) * mc.inverse(A[i]) + mc.inverse(A[i]) + 1) % MOD
+        ss.set(i, v)
+    print(ss.get(0) % MOD)
+
+
+class SegTree:
+    def __init__(
+        self,
+        op: "Callable[[Any, Any], Any]",
+        e: "Any",
+        v: "Union[int, List[Any]]",
+    ) -> None:
+        self._op = op
+        self._e = e
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = self.__class__._ceil_pow2(self._n)
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    @staticmethod
+    def _ceil_pow2(n: int) -> int:
+        x = 0
+        while (1 << x) < n:
+            x += 1
+        return x
+
+    def set(self, p: int, x: "Any") -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> "Any":
+        assert 0 <= p < self._n
+
+        return self._d[p + self._size]
+
+    def prod(self, left: int, right: int) -> "Any":
+        assert 0 <= left <= right <= self._n
+        sml = self._e
+        smr = self._e
+        left += self._size
+        right += self._size
+
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> "Any":
+        return self._d[1]
+
+    def max_right(self, left: int, f: "Callable[[Any], bool]") -> int:
+        assert 0 <= left <= self._n
+        assert f(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        sm = self._e
+
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not f(self._op(sm, self._d[left])):
+                while left < self._size:
+                    left *= 2
+                    if f(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, f: "Callable[[Any], bool]") -> int:
+        assert 0 <= right <= self._n
+        assert f(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        sm = self._e
+
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not f(self._op(self._d[right], sm)):
+                while right < self._size:
+                    right = 2 * right + 1
+                    if f(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+
+class ModComb:
+    def __init__(self, N, p: int = 1_000_000_007):
+        """\
+        :param N: 乗数の最大値。
+        :param p: 剰余に用いる数。このクラスが正しく機能するためには素数である必要がある
+        """
+        self._N = N
+        self._p = p
+        self._factorial_cache = {0: 1}
+        for i in range(1, N + 1):
+            self._factorial_cache[i] = (self._factorial_cache[i - 1] * i) % p
+
+    def factorial(self, n):
+        return self._factorial_cache[n]
+
+    def inverse(self, x):
+        """xの逆数 (mod p) を返す"""
+        # フェルマーの小定理からxの逆数をx'とすると x' = x^(p-2) mod p
+        k = self._p - 2
+        ret = 1
+        y = x
+        while k:
+            if k & 1:
+                ret = (ret * y) % self._p
+            y = (y * y) % self._p
+            k //= 2
+        return ret
+
+    def P(self, n, r) -> int:
+        """nPr mod p を求める"""
+        if n < r:
+            return 0
+        a = self.factorial(n)
+        b = self.factorial(n - r)
+        return (a * self.inverse(b)) % self._p
+
+    def C(self, n, r) -> int:
+        """nCr mod p を求める"""
+        if n < r:
+            return 0
+        a = self.factorial(n)
+        b = self.factorial(n - r)
+        c = self.factorial(r)
+        bc = (b * c) % self._p
+        return a * self.inverse(bc) % self._p
+
+    def H(self, n, r) -> int:
+        """\
+        nHr mod p (重複組合せ) を求める
+        """
+        if r == 0:
+            return 1
+        return self.C(n + r - 1, r)
 
 
 def main():
@@ -23,7 +211,7 @@ def main():
     tokens = iterate_tokens()
     N = int(next(tokens))  # type: int
     A = [int(next(tokens)) for _ in range(N - 1)]  # type: "List[int]"
-    solve(N, A)
+    solve_ref(N, A)
 
 
 if __name__ == "__main__":
