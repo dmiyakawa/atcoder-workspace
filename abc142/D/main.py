7dbe0fe98e30c08cd631cc072bfa3634bebba463
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-08-09 12:32:46 +0900

    Updates

---
+++ abc142/D/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import math
+import sys
+
+
+def solve(A: int, B: int):
+    M = math.gcd(A, B)
+    print((len(factorize_in_prime(M)) + 1) if M > 1 else 1)
+
+
+def factorize_in_prime(n) -> "Dict[int, int]":
+    """2以上の整数nを素因数分解し、{素因数: 指数, ...}の辞書を返す"""
+    # https://qiita.com/snow67675476/items/e87ddb9285e27ea555f8
+    assert n >= 2
+    d = {}
+    temp = n
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:
+            count = 0
+            while temp % i == 0:
+                count += 1
+                temp //= i
+            d[i] = count
+
+    if temp != 1:
+        d[temp] = 1
+
+    if not d:
+        d[n] = 1
+
+    return d
+
+def main():
+
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+
+    tokens = iterate_tokens()
+    A = int(next(tokens))  # type: int
+    B = int(next(tokens))  # type: int
+    solve(A, B)
+
+
+if __name__ == "__main__":
+    main()
