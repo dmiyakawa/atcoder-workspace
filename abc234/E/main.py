a63d966a6ef3608386dee91ec3003a94a56c6845
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-08-12 19:34:20 +0900

    Updates

--- abc234/E/main.py
+++ abc234/E/main.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
-import sys
 
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
+def solve(X: int):
+    lst = [int(e) for e in str(X)]
+    if len(lst) == 1:
+        return X
+
+    for i0 in range(lst[0], 10):
+        for i1 in range(10):
+            grad = i1 - i0
+            cand = [i0, i1] + [i1 + grad * n for n in range(1, len(lst) - 1)]
+            if any(v < 0 or v > 9 for v in cand):
+                continue
+            v = sum(n * 10 ** i for i, n in enumerate(reversed(cand)))
+            if v < X:
+                continue
+            return v
 
 
 def main():
-
-    def iterate_tokens():
-        for line in sys.stdin:
-            for word in line.split():
-                yield word
-
-    tokens = iterate_tokens()
-    X = int(next(tokens))  # type: int
-    solve(X)
-
-
-def solve(X: int):
-    return
+    print(solve(int(input())))
 
 
 if __name__ == "__main__":
