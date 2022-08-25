116e9a9d931e4363bd3bf32809dc9ed73413ce54
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-08-25 12:29:31 +0900

    Updates

--- abc075/B/main.py
+++ abc075/B/main.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
-import sys
-
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
-
 
 def solve(H: int, W: int, S: "List[str]"):
-    return
+    for i in range(H):
+        S[i] = list(S[i])
+
+    for i in range(H):
+        for j in range(W):
+            if S[i][j] == "#":
+                continue
+            count = 0
+            for h, w in [
+                (i - 1, j - 1), (i - 1, j), (i - 1, j + 1),
+                (i, j - 1), (i, j), (i, j + 1),
+                (i + 1, j -1), (i + 1, j), (i + 1, j + 1)
+            ]:
+                if h < 0 or w < 0 or h >= H or w >= W:
+                    continue
+                if S[h][w] == "#":
+                    count += 1
+            S[i][j] = str(count)
+    for h in range(H):
+        print("".join(S[h]))
 
 
 def main():
+    import sys
 
     def iterate_tokens():
         for line in sys.stdin:
--- abc229/D/main.py
+++ abc229/D/main.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 
-import sys
-
-sys.setrecursionlimit(2 * (10 ** 5))
-Inf = INF = float("INF")
+def solve(S: str, K: int):
+    lst = []
+    num = 0
+    for ch in S:
+        if ch == ".":
+            lst.append(num)
+            num = 0
+        else:
+            num += 1
+    lst.append(num)
+    ans = max(lst)
+    print(lst)
+    cur = 0
+    for l in range(len(lst)):
+        r = l + K + 1
+        if l == 0:
+            cur = sum(lst[l:min(r, len(lst))])
+        else:
+            cur -= lst[l - 1]
+            if r < len(lst):
+                cur += lst[r]
+            else:
+                r = len(lst)
+        print(l, r, lst[l:r], cur + r - l - 1)
+        ans = max(ans, cur + r - l - 1)
+    print(ans)
 
 
 def main():
+    import sys
 
     def iterate_tokens():
         for line in sys.stdin:
@@ -19,8 +42,6 @@ def main():
     solve(S, K)
 
 
-def solve(S: str, K: int):
-    return
 
 
 if __name__ == "__main__":
